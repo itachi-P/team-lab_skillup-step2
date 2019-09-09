258dc83863ae579b279b6232bc39f1694547d885
itachi-P <itachip38@gmail.com>
2019-09-09 22:46:51 +0900

    Maked GitHub social login function.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,13 +29,31 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * GitHubの認証ページヘユーザーをリダイレクト
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->scopes(['read:user', 'public_repo'])->redirect(); 
+    }
+
+    /**
+     * GitHubからユーザー情報を取得
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback(Request $request)
+    {
+        $user = Socialite::driver('github')->user();
+
+        $request->session()->put('github_token', $user->token);
+        return redirect('github');
     }
 }
