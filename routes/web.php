0881438d7620ea2b382c7c196821d40f35e6461b
itachi-P <itachip38@gmail.com>
2019-09-11 06:55:07 +0900

    :sunrise_over_mountains: Upload an image.

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('login/github', 'Auth\LoginController@redirectToProvider');
 Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
 
 Route::post('user', 'User\UserController@updateUser');
+
+Route::get('/', 'HomeController@index');
