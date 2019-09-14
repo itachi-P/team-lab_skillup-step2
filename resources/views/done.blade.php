53442426ea2fcec10ac7ec58dce1b159d34e0313
itachi-P <itachip38@gmail.com>
2019-09-14 20:43:46 +0900

    :panda_face: set favicon to other pages

--- resources/views/done.blade.php
+++ resources/views/done.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" href="{{ asset('fuca_tehepero_icon_32x32.ico') }}" sizes="32x32">
         <title>github</title>
     </head>
     <body>
